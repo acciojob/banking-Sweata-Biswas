fe1e2b16d9d0aa53c329c101ab269bc50a3bbb86
Sweata-Biswas <biswassweata22@gmail.com>
2022-12-30 23:32:50 +0530

    final

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -15,14 +15,4 @@ public class StudentAccount extends BankAccount{
         return institutionName;
     }
 
-<<<<<<< HEAD
-    public String getInstitutionName() {
-        return institutionName;
-    }
-
-=======
->>>>>>> 4a52badb5959c09c67965fe0ee4911f27ca741ee
-    public void setInstitutionName(String institutionName) {
-        this.institutionName = institutionName;
-    }
 }
